9799182f97d2c298cfd5296ceec417e9dbf7c6df
Jordan Walsh <jordanwalsh23@hotmail.com>
2016-02-07 20:37:11 +1100

    removed shortcut plugin

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.plugins.shortcut/www/ShortcutPlugin.js",
-        "id": "com.plugins.shortcut.ShortcutPlugin",
-        "pluginId": "com.plugins.shortcut",
-        "clobbers": [
-            "ShortcutPlugin"
-        ]
-    },
-    {
         "file": "plugins/phonegap-plugin-push/www/push.js",
         "id": "phonegap-plugin-push.PushNotification",
         "pluginId": "phonegap-plugin-push",
@@ -34,7 +26,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.plugins.shortcut": "0.1.1",
     "phonegap-plugin-push": "1.5.3",
     "cordova-plugin-device": "1.1.1",
     "cordova-plugin-whitelist": "1.2.1"
