4ccc4c7d564d9d2c698a5410a336a0ec0c925631
Jordan Walsh <jordanwalsh23@hotmail.com>
2016-02-07 20:02:44 +1100

    fixed plugin packages

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,17 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
+        "file": "plugins/com.plugins.shortcut/www/ShortcutPlugin.js",
+        "id": "com.plugins.shortcut.ShortcutPlugin",
+        "pluginId": "com.plugins.shortcut",
         "clobbers": [
-            "device"
+            "ShortcutPlugin"
         ]
     },
     {
@@ -23,21 +17,27 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/com.plugins.shortcut/www/ShortcutPlugin.js",
-        "id": "com.plugins.shortcut.ShortcutPlugin",
-        "pluginId": "com.plugins.shortcut",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
         "clobbers": [
-            "ShortcutPlugin"
+            "device"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-device": "1.1.1",
+    "com.plugins.shortcut": "0.1.1",
     "phonegap-plugin-push": "1.5.3",
-    "com.plugins.shortcut": "0.1.1"
+    "cordova-plugin-device": "1.1.1",
+    "cordova-plugin-whitelist": "1.2.1"
 }
 // BOTTOM OF METADATA
 });
